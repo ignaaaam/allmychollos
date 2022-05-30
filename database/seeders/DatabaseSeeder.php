ee660d045e67ec726d22cb6891c5fb0782222607
iamatnuria <ignasiamat10@gmail.com>
2022-05-30 16:52:55 +0200

    Category seeding fixed.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,9 +36,57 @@ class DatabaseSeeder extends Seeder
             'role_id' => 1
         ]);
 
-        $discount = Discount::factory(25)->create([
-            'user_id' => $user->id,
-            'premium' => false
-        ]);
+        $categories = [
+            [
+                'name' => 'Electrónica',
+                'slug' => 'electronica'
+            ],
+            [
+                'name' => 'Gaming',
+                'slug' => 'gaming'
+            ],
+            [
+                'name' => 'Supermercado y alimentacion',
+                'slug' => 'supermercado-y-alimentacion'
+            ],
+            [
+                'name' => 'Moda y accesorios',
+                'slug' => 'moda-y-accesorios'
+            ],
+            [
+                'name' => 'Salud y belleza',
+                'slug' => 'salud-y-belleza'
+            ],
+            [
+                'name' => 'Juguetes y familia',
+                'slug' => 'juguetes-y-familia'
+            ],
+            [
+                'name' => 'Hogar, vivienda y oficina',
+                'slug' => 'hogar-vivienda-y-oficna'
+            ],
+            [
+                'name' => 'Jardín y bricolaje',
+                'slug' => 'jardin-y-bricolaje'
+            ],
+            [
+                'name' => 'Deportes y aire libre',
+                'slug' => 'deportes-y-aire-libre'
+            ],
+            [
+                'name' => 'Software',
+                'slug' => 'software'
+            ]
+        ];
+
+        foreach ($categories as $category) {
+            $category = Category::create($category);
+
+            Discount::factory()->create([
+                'user_id' => $user->id,
+                'premium' => false,
+                'category_id' => $category->id,
+            ]);
+        }
     }
 }
