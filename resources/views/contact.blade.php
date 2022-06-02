1c70aebe47ef34310bdb93bbacf1db59713812ab
iamatnuria <ignasiamat10@gmail.com>
2022-06-02 15:50:54 +0200

    FAQ and Contact page added

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <section class="flex justify-center items-center container mx-auto">
+        <div class="p-32">
+            <h1 class="text-4xl font-bold">Contacto</h1>
+            <p class="py-4">Envíanos un mensaje a la dirección <i>info@allmychollos.com</i></p>
+    </section>
+</x-layout>
--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,3 +1,15 @@
 <x-layout>
-    FAQ
+    <section class="flex justify-center items-center container mx-auto">
+        <div class="p-8">
+            <h1 class="text-4xl font-bold">¿Que es esta página web?</h1>
+            <p class="py-4">AllMychollos es una pagina web de descuentos donde los usuarios pueden encontrar los mejores descuentos de diferentes paginas web.
+            Los usuarios también pueden crear sus descuentos, editarlos y eliminarlos.</p>
+
+            <h1 class="text-4xl font-bold">¿Es gratuïta?</h1>
+            <p class="py-4">De momento si, aunque habrá un sistema de suscripciones que permitirá a los usuarios ver mejores descuentos si pagan una mensualidad (el precio todavia está por determinar)</p>
+
+            <h1 class="text-4xl font-bold">¿Que tipo de descuentos encontraré</h1>
+            <p class="py-4">En Allmychollos encontrarás descuentos divididos en muchas categorias, desde Gamind, Electronica, hasta Belleza, cosas para el Jardin o el Hogar entre muchos otros...</p>
+        </div>
+    </section>
 </x-layout>
