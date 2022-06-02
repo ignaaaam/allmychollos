0df5c7dc5b468d61cd8f121e6218fd150afe5212
iamatnuria <ignasiamat10@gmail.com>
2022-06-02 18:36:13 +0200

    Best discounts set to >= 25%

--- resources/views/discounts/index.blade.php
+++ resources/views/discounts/index.blade.php
@@ -5,10 +5,10 @@
             :categories="$categories" />
         <section class=" w-full text-center h-3/4 bg-light-gray flex flex-col items-center justify-center p-10">
             <h1 class="uppercase text-main-gray text-4xl font-bold">Mejores Descuentos</h1>
-    @if($discounts->count())
+        @if($discounts->count())
             <div class="swiper">
                 <div class="swiper-wrapper flex items-center">
-                    @foreach($discounts->where('percentage','>','25') as $discount)
+                    @foreach($discounts->where('percentage','>=','25') as $discount)
                         <x-discount-featured-card :discount="$discount" />
                     @endforeach
                 </div>
