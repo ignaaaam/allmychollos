9798d83cc8faff028bca9a72a668aac629c302b6
iamatnuria <ignasiamat10@gmail.com>
2022-06-03 11:15:29 +0200

    Typo fixed in FAQ

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,11 +5,11 @@
             <p class="py-4">AllMychollos es una pagina web de descuentos donde los usuarios pueden encontrar los mejores descuentos de diferentes paginas web.
             Los usuarios también pueden crear sus descuentos, editarlos y eliminarlos.</p>
 
-            <h1 class="text-4xl font-bold">¿Es gratuïta?</h1>
+            <h1 class="text-4xl font-bold mt-4">¿Es gratuïta?</h1>
             <p class="py-4">De momento si, aunque habrá un sistema de suscripciones que permitirá a los usuarios ver mejores descuentos si pagan una mensualidad (el precio todavia está por determinar)</p>
 
-            <h1 class="text-4xl font-bold">¿Que tipo de descuentos encontraré</h1>
-            <p class="py-4">En Allmychollos encontrarás descuentos divididos en muchas categorias, desde Gamind, Electronica, hasta Belleza, cosas para el Jardin o el Hogar entre muchos otros...</p>
+            <h1 class="text-4xl font-bold mt-4">¿Que tipo de descuentos encontraré</h1>
+            <p class="py-4">En Allmychollos encontrarás descuentos divididos en muchas categorias, desde Gaming, Electrónica, hasta Belleza, cosas para el Jardin o el Hogar, entre muchos otros...</p>
         </div>
     </section>
 </x-layout>
