08fda2a1ce61e76b59f589751babef9e401d4beb
iamatnuria <ignasiamat10@gmail.com>
2022-06-04 17:41:08 +0200

    translate=no added on HTML to avoid translation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="scroll-smooth" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="scroll-smooth" translate="no" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
